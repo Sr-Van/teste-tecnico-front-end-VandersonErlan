b6147f884e222acca4c54d06b9cc73496f16ee3b
Vanderson Erlan <vandersonerlan@Vandersons-MBP.bbrouter>
2024-06-27 13:02:37 -0300

    feat: filtros de preço maximo e minimo & removendo linhas desnecessarias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,7 +21,6 @@ export class AppComponent {
   public utS$ = inject(UtilService)
 
   constructor() {
-    this.utS$.runLoading();
   }
 
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 
 import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/types/types';
+import { UtilService } from '../../shared/services/util.service';
 
 @Component({
   selector: 'app-home',
@@ -13,23 +14,54 @@ import { Product } from '../../shared/types/types';
 export class HomeComponent {
 
   private prodS$ = inject(ProductsService);
+  public utS$ = inject(UtilService)
 
   public prodArray: Product[] = [];
+  public filteredProdArray: Product[] = [];
+  public selectValue: number = 0;
 
   constructor() {
     this.getProds();
   }
 
-  getProds(): void {
+  private getProds(): void {
     this.prodS$.getProducts().subscribe(
       {
         next: (data) => {
           this.prodArray = data
+          this.filteredProdArray = this.prodArray
         },
         error: (err) => {
           console.log(err)
         }
       }
     )
+  }
+
+  public handleSelectChange(event: Event): void {
+
+    this.selectValue = Number((event.target as HTMLInputElement).value)
+
+    switch (this.selectValue) {
+      case 0:
+        this.filteredProdArray = this.prodArray
+        break
+      case 1:
+        this.sortByPriceMin()
+        break
+      case 2:
+        this.sortByPriceMax()
+        break
+    }
+  }
+
+  private sortByPriceMax(): void {
+    this.utS$.runLoading();
+    this.filteredProdArray = this.prodArray.sort((a, b) => b.price - a.price)
+  }
+
+  private sortByPriceMin(): void {
+    this.utS$.runLoading();
+    this.filteredProdArray = this.prodArray.sort((a, b) => a.price - b.price)
   }
 }
