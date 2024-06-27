7e3337d5011c437e0fd8c8a79fc6bd2efc8eeb28
Vanderson Erlan <vandersonerlan@Vandersons-MBP.bbrouter>
2024-06-27 10:55:59 -0300

    feat: loading no app component para ser usado globalmente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { HeaderComponent } from './shared/components/header/header.component';
 
 import { RouterOutlet, RouterModule } from '@angular/router';
+import { LoadingComponent } from './shared/components/loading/loading.component';
+import { UtilService } from './shared/services/util.service';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, HeaderComponent],
+  imports: [RouterOutlet, RouterModule, HeaderComponent,
+    LoadingComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'teste-tecnico-front-end-vanderson';
+
+  public utS$ = inject(UtilService)
+
+  constructor() {
+    this.utS$.runLoading();
+  }
+
 }
