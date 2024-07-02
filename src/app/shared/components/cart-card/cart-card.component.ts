c34318690d46bf3c6a06a5ef5f8b342964e56f21
Vanderson Erlan <vandersonerlan@Vandersons-MBP.bbrouter>
2024-07-02 11:37:43 -0300

    feat: integrando remover item no card do cart. styles: cart card & responsividade na cart page com resumo criado

--- src/app/shared/components/cart-card/cart-card.component.ts
+++ src/app/shared/components/cart-card/cart-card.component.ts
@@ -1,15 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 import { Product } from '../../types/types';
+import { CartService } from '../../services/cart.service';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'c-card',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './cart-card.component.html',
   styleUrl: './cart-card.component.css'
 })
 export class CartCardComponent {
+  private cart$ = inject(CartService);
 
   @Input() product: Product
+
+  public removeItem(id: number): void {
+    this.cart$.removeItemFromCart(id);
+  }
+
+  public calcTotal(price: number, quantity: number): number {
+    return price * quantity;
+  }
 
 }
