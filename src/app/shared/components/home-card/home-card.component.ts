817f5379a3d8ca3bfc7abba821ec4d0c7fd74b6e
Vanderson Erlan <vandersonerlan@Vandersons-MBP.bbrouter>
2024-07-08 11:40:34 -0300

    feat: integrando wishlist no homecard component

--- src/app/shared/components/home-card/home-card.component.ts
+++ src/app/shared/components/home-card/home-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, inject } from '@angular/core';
+import { AfterViewInit, Component, Input, inject } from '@angular/core';
 import { CurrencyPipe } from '@angular/common';
 
 import { Product } from '../../types/types';
@@ -12,12 +12,19 @@ import { UtilService } from '../../services/util.service';
   templateUrl: './home-card.component.html',
   styleUrl: './home-card.component.css'
 })
-export class HomeCardComponent {
+export class HomeCardComponent implements AfterViewInit{
 
   private cart$ = inject(CartService);
   private utS$ = inject(UtilService);
 
   @Input() product: Product;
+  public isItemOnWishList: boolean = false;
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.isItemOnWishList = this.utS$.wishlist.some((prod) => prod.id === this.product.id);
+    }, 400);
+  }
 
   public addItemToCart(product: Product): void {
     this.cart$.addItemToCart(product);
@@ -25,5 +32,17 @@ export class HomeCardComponent {
     this.cart$.getCartLength();
 
     this.utS$.runDriver();
+  }
+
+  public addToWishlist(product: Product): void {
+
+    if(this.isItemOnWishList) {
+      this.utS$.removeToWishlist(product.id);
+      this.isItemOnWishList = false;
+      return
+    }
+
+    this.isItemOnWishList = true;
+    this.utS$.addToWishlist(product);
   }
 }
